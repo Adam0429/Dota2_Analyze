8e47ce6856ca5c0ff111f712a27dfd7a5496427b
Adam0429 <872490934@qq.com>
2020-02-28 01:56:54 +0000

    Assignment

--- a1.py
+++ a1.py
@@ -1,38 +1,40 @@
 import requests
 from tqdm import tqdm
-import jieba
 import IPython
 from wordcloud import WordCloud
-import os
-import time
-
-my_id = '244473233'
+import csv
+import pandas as pd 
 
 # 分析 推塔 杀人的关系
 def alive_towers(n):
 	return str(bin(n))[2:].count('1')
-def get_matches(user_id):
-	url = 'https://api.opendota.com/api/players/%s/Matches?limit=500&'%(user_id)
+
+def getMatchId(user_id,number):
+	url = 'https://api.opendota.com/api/players/%s/Matches?limit=%s'%(user_id,str(number))
 	matches = requests.get(url).json()
 	match_ids = [match['match_id'] for match in matches]
 	return match_ids
 
-def get_match_data(match_id):
+def getMatchData(match_id):
 	match_url = 'https://api.opendota.com/api/matches/%s'%(match_id)
 	match_data = requests.get(match_url).json()
 	# if match_data['radiant_win'] == True:
-	print(match_data['radiant_score']-match_data['dire_score'],match_data['radiant_win'])
-	print(alive_towers(match_data['tower_status_radiant']),alive_towers(match_data['tower_status_dire']))
-	print(match_data['duration'])
+	# print(match_data['radiant_score']-match_data['dire_score'],match_data['radiant_win'])
+	# print(alive_towers(match_data['tower_status_radiant']),alive_towers(match_data['tower_status_dire']))
+	# print(match_data['duration'])
 	return match_data
 
 
+my_id = '904606353' #我
 
-match_ids = get_matches(my_id)
+match_ids = getMatchId(my_id,1400)
+raw_data = []
 for match_id in tqdm(match_ids):
-	# try:
-	get_match_data(match_id)
-	# except:
+	raw_data.append(getMatchData(match_id))
+IPython.embed()
+df = pd.DataFrame(raw_data,index=None)
+df.to_csv('raw_data.csv')
+# except:
 	# 	pass
 # get_chat(_id)
 
--- test.py
+++ test.py
@@ -24,71 +24,71 @@ def get_matches(id):
 	matches_id = requests.get(url).json
 	print(matches_id.text)
 
-# def output_cloud(id):
-#     count = {}
-#     file = open(id+'_chat.txt','r+')
-#     chats = file.readlines()
-#     for chat in chats:
-#         words = jieba.cut(chat.strip(), cut_all=True)
-#         for word in words:
-#             if word not in count.keys():
-#                 count[word] = 1
-#             else:
-#                 count[word] += 1
-#     text = '' 
-#     for key,value in count.items():
-#         for i in range(value):
-#             text += key+' '
-#     wc = WordCloud(
-#         width=1000,
-#         height=600,
-#         font_path='STZHONGS.TTF',
-#         max_font_size=100,      #字体大小
-#         min_font_size=30,
-#         collocations=False, 
-#         max_words=40 
-#     )
-#     wc.generate(text)
-#     wc.to_file(_id+'.png') #图片保存
+def output_cloud(id):
+    count = {}
+    file = open(id+'_chat.txt','r+')
+    chats = file.readlines()
+    for chat in chats:
+        words = jieba.cut(chat.strip(), cut_all=True)
+        for word in words:
+            if word not in count.keys():
+                count[word] = 1
+            else:
+                count[word] += 1
+    text = '' 
+    for key,value in count.items():
+        for i in range(value):
+            text += key+' '
+    wc = WordCloud(
+        width=1000,
+        height=600,
+        font_path='STZHONGS.TTF',
+        max_font_size=100,      #字体大小
+        min_font_size=30,
+        collocations=False, 
+        max_words=40 
+    )
+    wc.generate(text)
+    wc.to_file(_id+'.png') #图片保存
 
 
-# def analyze_matchs(id,reverse=False):
-#     headers = {
-#         'Sec-Fetch-Mode': 'cors',
-#         'Referer': 'https://www.opendota.com/request',
-#         'Origin': 'https://www.opendota.com',
-#         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
-#     }
-#     matches = requests.get('https://api.opendota.com/api/players/'+id+'/matches').json()
-#     match_ids = [match['match_id'] for match in matches]
-#     if reverse:
-#         match_ids.sort()
-#     for match_id in tqdm(match_ids):
-#         response = requests.post('https://api.opendota.com/api/request/'+str(match_id), headers=headers)
-#         time.sleep(2)
+def analyze_matchs(id,reverse=False):
+    headers = {
+        'Sec-Fetch-Mode': 'cors',
+        'Referer': 'https://www.opendota.com/request',
+        'Origin': 'https://www.opendota.com',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
+    }
+    matches = requests.get('https://api.opendota.com/api/players/'+id+'/matches').json()
+    match_ids = [match['match_id'] for match in matches]
+    if reverse:
+        match_ids.sort()
+    for match_id in tqdm(match_ids):
+        response = requests.post('https://api.opendota.com/api/request/'+str(match_id), headers=headers)
+        time.sleep(2)
 
-# def get_chat(id):
-#     matches = requests.get('https://api.opendota.com/api/players/'+id+'/matches').json()
-#     file = open(id+'_chat.txt','w')
-#     for match in tqdm(matches):
-#         match_info = requests.get(match_url+str(match['match_id'])).json()
-#         if 'players' not in match_info.keys():
-#             continue
-#         players = match_info['players']
-#         for player in players:
-#             if str(player['account_id']) == id:
-#                 if 'player_slot' in player.keys():
-#                     player_slot = player['player_slot']
-#                     break
-#         if 'chat' not in match_info.keys():
-#             continue
-#         if match_info['chat'] != None:
-#             for chat in match_info['chat']:
-#                 if chat['slot'] == player_slot and chat['type'] != 'chatwheel':
-#                     file.write(chat['key']+'\n')
-#                     print(chat['key'])
+def get_chat(id):
+    matches = requests.get('https://api.opendota.com/api/players/'+id+'/matches').json()
+    file = open(id+'_chat.txt','w')
+    for match in tqdm(matches):
+        match_info = requests.get(match_url+str(match['match_id'])).json()
+        if 'players' not in match_info.keys():
+            continue
+        players = match_info['players']
+        for player in players:
+            if str(player['account_id']) == id:
+                if 'player_slot' in player.keys():
+                    player_slot = player['player_slot']
+                    break
+        if 'chat' not in match_info.keys():
+            continue
+        if match_info['chat'] != None:
+            for chat in match_info['chat']:
+                if chat['slot'] == player_slot and chat['type'] != 'chatwheel':
+                    file.write(chat['key']+'\n')
+                    print(chat['key'])
 
-# analyze_matchs(_id,reverse=True)
+analyze_matchs(_id,reverse=True)
 get_matches(_id)
 # get_chat(_id)
 
